db1e90fcd7d3582fdd55450600ed6830600b6a5c
dagreatcode <dagreatcode@gmail.com>
2021-03-30 11:04:14 -0400

    added the view controller

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Controllers
+def home(request):
+    response = "<h1>Hey Ma! Look at this here Django"
+    return HttpResponse(response)
